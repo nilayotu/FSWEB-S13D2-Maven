4c2c9391ea2e3cae276105c0b280efc0a95becf8
nilayotu <nilayotu_07@hotmail.com>
2025-07-01 18:05:24 +0300

    task-1, task-2 has been completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,39 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    public static boolean isPalindrome(int a) {
+
+        int absValue = Math.abs(a);
+        String original = String.valueOf(absValue);
+        String reversed = new StringBuilder(original).reverse().toString();
+        return original.equals(reversed);
+    }
+
+    public static boolean isPerfectNumber(int x){
+        if (x < 0) {
+            return false;
+        }
+
+        int sum = 0;
+
+        for (int i=1; i<x; i++) {
+            if (x % i ==0) {
+                sum=sum+i;
+            }
+        }
+        return sum == x;
+    }
+
+    public static String numberToWords(int p) {
+        if (p < 0) {
+            return "invalid Value";
+        }
+
+
+    }
+
 }
+
+
